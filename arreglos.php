f1871e4a7a47fbccbe98a4d7b34efffe47704952
Lemus <lemusangel1092@hotmail.com>
2021-09-07 10:10:27 -0500

    Se me olvido agregar un archivo xD

--- arreglos.php
+++ arreglos.php
@@ -1,7 +1,12 @@
 <?php
+
+#permite almacernar multiples valores
 $semana = array('lunes','martes','miercoles','jueves','viernes','sabado','domingo');
 $semana[1] = 'LemusCode';
 
 echo $semana[1];
+
+
+$semana2 = ['lunes','martes','miercoles','jueves','viernes','sabado','domingo'];
 
 ?>
