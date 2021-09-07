35e0b7c7945a8f3e6489374bc5fdf14683890f63
Lemus <lemusangel1092@hotmail.com>
2021-09-07 12:14:28 -0500

    Clase de condicionales, ahora voy a clase de Git

---
+++ condicionales.php
@@ -0,0 +1,12 @@
+<?php
+$edad = 18;
+$nombre = 'Lemus';
+
+if ($edad >= 18 && $nombre == 'Lemus') {
+    echo '<h1> Bienvenido </h1>';
+}
+
+
+if ($edad < 18  $nombre != 'Lemus') {
+    echo '<h1> Menor de edad, Lemus </h1>';
+}
