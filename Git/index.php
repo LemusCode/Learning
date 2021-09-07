401c2e188e05281b356cc5dd1887fbff7cf58b66
Lemus <lemusangel1092@hotmail.com>
2021-09-07 18:54:13 -0500

    Subiendo todo

--- Git/index.php
+++ Git/index.php
@@ -18,15 +18,8 @@
 </head>
 <body>
     <input type="button" onclick="saludo()" value="Picale">
-<<<<<<< HEAD
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ab et laudantium nobis aliquam ex amet natus explicabo deleniti iste!</p>
-=======
-
-
-
-
-
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ab et laudantium nobis aliquam ex amet natus explicabo deleniti iste! - Texto de branch 2</p>
+    
     <p>Lorem, ipsum dolor. Desde Javascript Branch</p>
->>>>>>> javascript
 </body>
 </html>
