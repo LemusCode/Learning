6356cc8126f876b36f5149b9571d6b98cb2ca2c8
Lemus <lemusangel1092@hotmail.com>
2021-09-07 17:12:08 -0500

    Agreando JS y poniendo boton de saludo

--- Git/index.php
+++ Git/index.php
@@ -6,3 +6,16 @@
 
 #Aqui agregamos mas para mandar un push
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="./javascript/javascript.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <input type="button" onclick="saludo()" value="Picale">
+</body>
+</html>
