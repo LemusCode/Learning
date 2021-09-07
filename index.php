622cfecc1410d97697f9eacae5b9c70a9dff180a
Lemus <lemusangel1092@hotmail.com>
2021-09-07 09:35:32 -0500

    Clase Variables y agregando clase Arreglos

--- index.php
+++ index.php
@@ -1,5 +1,33 @@
 <?php
-echo "Holo";
 
-$variable = "Veamos";
+#string: Cadena de texto
+$nombre = "Lemus";
+
+#integer: Numeor enteros
+$numero = 7;
+
+#Double: numero decimal
+$decimal = 7.7;
+
+#boleano: Verdadero o falso
+$bool = false;
+
+#array: Arreglo
+#Object. Objeto
+#Clasee: Clase
+#Null: Una variable sin valor
+
+#diferencia entre comillas dobes y sencillas
+#las comillas sobles nos permiten llamar variables
+#las comillas simples no permiten ejecutar variables
+
+#Lo idea es usar comillas sencillas, excepto cuando 
+#se quiera el valor de una variable
+#Las comillas simples facilitan la seguridad en PHP
+#echo "Holo $nombre </br>";
+
+#echo 'Holo' . $nombre . '</br>';
+
+
+echo gettype($numero);
 ?>
