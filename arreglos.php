e790d2fca2f8892789dd11cb4b0883f40bc7486f
Lemus <lemusangel1092@hotmail.com>
2021-09-07 10:09:40 -0500

    Clase de arreglos

---
+++ arreglos.php
@@ -0,0 +1,7 @@
+<?php
+$semana = array('lunes','martes','miercoles','jueves','viernes','sabado','domingo');
+$semana[1] = 'LemusCode';
+
+echo $semana[1];
+
+?>
---
+++ constantes.php
@@ -0,0 +1,13 @@
+<?php
+
+define('PI', 3.14);
+define('NOMBRE', 'Lemus');
+echo NOMBRE;
+
+#las cosntantes no se pueden sobre escribir
+#una vez definidas no se puedes cambiar 
+#Tiene ventaja de que son variable globales
+#Se puede acceder a ellas desde cualquier parte del codigo
+#Las constantes no llevan comillas, solo se llaman y ya
+
+?>
