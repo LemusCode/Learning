4bc6c32f68854c2df5b7ab8a9daf82a78a3fe7bc
Lemus <lemusangel1092@hotmail.com>
2021-09-08 12:13:04 -0500

    Clase operadores en PHP

---
+++ PHP/operadores.php
@@ -0,0 +1,50 @@
+<?php
+#operarodres aritmeticos
+# + - * / %
+
+/*Operadores de asignacion
+
+=  asigna un valor a una variable $variable = 5
++= Variable en si misma mas algun valor $variable += 5 esto da 10
+-= Variable en si misma menos algun valor $variable -= 5 esto da 0
+*= Variable en si misma por algun valor $variable *= 5 esto da 0
+/= Variable en si misma dividida por algun valor $variable /= 5 da 1
+
+*/
+
+/*Operadores de comparacion
+
+==      Igualdad
+===     Identico (En valor y tipo de dato)
+!=, <>  Diferente
+<=      Menor o igual
+>=      Mayor o igual
+<       Menos
+>       Mayor
+
+*/
+
+/* Operadores Logicos
+
+And, &&
+Or, ||
+xor
+!
+
+*/
+
+/*Operadores de incremento y decremento
+
+++$x    $numero++ aumenta 1 unidad -> La operacion de hace antes de tomar el valor de la variable
+$x++    $numero++ aumenta 1 unidad
+--$x    $numero++ disminuye 1 unidad -> La operacion de hace antes de tomar el valor de la variable
+$x--    $numero++ disminuye 1 unidad
+
+*/
+
+/*Operadores de cadena
+
+.  $variable . 'Concatenar texto' . $Otra variable;
+.= $variable1 = 'Eso es una Cadena'; $variable1 .= 'de texto para ejemplo';
+
+*/
