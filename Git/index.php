2ac78d34fd28af5c86e3d2b00dd236be71bbb070
LemusCode <31278486+LemusCode@users.noreply.github.com>
2021-09-07 19:28:29 -0500

    para otro merge

--- Git/index.php
+++ Git/index.php
@@ -14,7 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="./javascript/javascript.js"></script>
     <link rel="stylesheet" href="./css/estilos.css">    
-    <title>Curso Git</title>
+    <title>Curso Git - Otro merge</title>
 </head>
 <body>
     <input type="button" onclick="saludo()" value="Picale">
