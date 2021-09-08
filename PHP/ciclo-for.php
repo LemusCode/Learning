1e8fa9ae592c89e814455fa2450540f9151897fd
Lemus <lemusangel1092@hotmail.com>
2021-09-08 12:37:55 -0500

    Ciclo for final

--- PHP/ciclo-for.php
+++ PHP/ciclo-for.php
@@ -4,3 +4,16 @@
 for ($i = 1; $i <= 10; $i++) {
     echo $i . '<br>';
 }
+
+/* Del 10 al 1
+for ($i = 10; $i >= 10; $i--) {
+    echo $i . '<br>';
+}
+*/
+
+/* Incrementando de 5 en 5
+for ($i = 0; $i <= 10; $i + 5) {
+    echo $i . '<br>';
+}
+*/
+
