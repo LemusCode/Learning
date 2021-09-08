dde5829bd8c71d6bd17f87a490aa83d11c43d78e
LemusCode <31278486+LemusCode@users.noreply.github.com>
2021-09-07 19:27:34 -0500

    Para poder hacer merge

--- Git/index.php
+++ Git/index.php
@@ -14,7 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="./javascript/javascript.js"></script>
     <link rel="stylesheet" href="./css/estilos.css">    
-    <title>Curso Git</title>
+    <title>Curso Git - Para Merge</title>
 </head>
 <body>
     <h1>Titulo para la pagina</h1>
