0181de226d1007db24f70a0ba7bdbaa371c1f841
Lemus <lemusangel1092@hotmail.com>
2021-09-08 12:18:27 -0500

    Clase Switch

---
+++ PHP/switch.php
@@ -0,0 +1,14 @@
+<?php
+$mes = 'Diciembre';
+
+switch ($mes) {
+    case 'Diciembre':
+        echo "Feliz navidad";
+        break;
+    case 'Enero':
+        echo "Feliz año nuevo";
+        break;
+    default:
+        echo "Sin celebracion";
+        break;
+}
